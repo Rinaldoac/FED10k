2c3fe357b0d0472815e7012e0c3ba6800446df42
Rinaldo Costa <rinaldo_azeredo@hotmail.com>
2022-10-31 18:05:42 -0300

    manut no anima scroll

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -3,9 +3,9 @@ import debounce from './debounce.js';
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.windowMetade = window.innerHeight * 0.6;
+    this.windowMetade = window.innerHeight * 0.75;
 
-    this.checkDistance = debounce(this.checkDistance.bind(this), 60);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 70);
   }
 
   // Pega a distância de cada item em relação
