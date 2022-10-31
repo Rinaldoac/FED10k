f7599bc66c7a5e5714cc76e856d2f44cddd1e4dd
Rinaldo Costa <rinaldo_azeredo@hotmail.com>
2022-10-31 19:00:37 -0300

    scroll e escrita button

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -3,9 +3,9 @@ import debounce from './debounce.js';
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.windowMetade = window.innerHeight * 0.75;
+    this.windowMetade = window.innerHeight * 1;
 
-    this.checkDistance = debounce(this.checkDistance.bind(this), 70);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 1);
   }
 
   // Pega a distância de cada item em relação
